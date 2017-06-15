6341e65ea3a472389567eb4746276b3ea1998bfb
KAPAKAYALA ANJI BABU <anjibabu480@gmail.com>
2017-06-15 19:55:30 +0530

    Add files via upload

---
+++ sendmail_V2.sh
@@ -0,0 +1,161 @@
+#!/bin/bash
+#This script is to send a mail from PC via GUI interface shell script. (Xdialog)
+#
+#Authour:  ANJI BABU KAPAKYALA
+#          IIT KANPUR, INDIA.
+#
+#Creating dummy file
+OUTPUT="/tmp/input.txt"
+# create empty file
+>$OUTPUT
+# cleanup  - add a trap that will remove $OUTPUT
+# if any of the signals - SIGHUP SIGINT SIGTERM it received.
+trap "rm $OUTPUT; exit" SIGHUP SIGINT SIGTERM
+#-----------Get mailid------------------------------
+Xdialog --title "MAIL ID" \
+--backtitle "Welcome to ANJI BABU KAPAKAYALA mailing services." \
+--inputbox "Enter mailid " 8 60 2>$OUTPUT
+# get respose
+respose=$?
+# make a decsion 
+case $respose in
+  0)
+       echo "OK pressed" 
+        ;;
+  1)
+       exit
+#        echo "Cancel pressed." 
+        ;;
+  255)  
+       #   echo "[ESC] key pressed."
+esac
+# get data stored in $OUPUT using input redirection
+mailid=$(<$OUTPUT)
+sed -i "/^/d" $OUTPUT
+#cat $OUTPUT
+#-----------Get Subject------------------------------#
+#writing Subject
+Xdialog --title "SUBJECT" \
+--backtitle "ANJI BABU KAPAKAYALA" \
+--inputbox "Enter Subject " 8 60 2>$OUTPUT
+respose=$?
+# make a decsion 
+case $respose in
+  0)
+       echo "OK pressed" 
+        ;;
+  1)
+       exit
+#        echo "Cancel pressed." 
+        ;;
+  255)  
+       #   echo "[ESC] key pressed."
+esac
+subject=$(<$OUTPUT)
+sed -i "/^/d" $OUTPUT
+#-----------Get Message------------------------------#
+#writteng Message 
+Xdialog --title "MESSAGE" \
+--backtitle "ANJI BABU KAPAKAYALA" \
+--inputbox "Type your Message Here: " 8 60 2>$OUTPUT
+respose=$?
+# make a decsion 
+case $respose in
+  0) 
+# Ok Pressed
+  	;;
+  1) 
+        exit
+#  	echo "Cancel pressed." 
+  	;;
+  255) 
+#   echo "[ESC] key pressed."
+esac
+message=$(<$OUTPUT)
+sed -i "/^/d" $OUTPUT
+#-----------Ask for Attachments---------------------------#
+Xdialog --backtitle "ANJI BABU KAPAKAYALA" \
+--title " Any Attachments ?" \
+--yesno "Do You want to attach any file ?  " 8 60 
+respose=$?
+# make a decsion 
+case $respose in
+  0) 
+#OK pressed
+choice="yes"
+#---------------------------------
+Xdialog --title " ATTACHMENTS " \
+--backtitle "ANJI BABU KAPAKAYALA" \
+--inputbox "Give Your Attachemt: " 8 60 2>$OUTPUT
+respose=$?
+# make a decsion 
+case $respose in
+  0) 
+#  Ok Pressed
+  	;;
+  1) 
+        exit
+#  	echo "Cancel pressed." 
+  	;;
+  255) 
+#   echo "[ESC] key pressed."
+esac
+attachment=$(<$OUTPUT)
+sed -i "/^/d" $OUTPUT
+#----------------------------------
+  	;;
+  1) 
+ #echo "Cancel pressed." 
+  choice="no"
+  	;;
+  255) 
+#   echo "[ESC] key pressed."
+esac
+#-----------Get Confiramation-------------------------#
+Xdialog --yes-label "Send" \
+--backtitle "ANJI BABU KAPAKAYALA" \
+--title "Confirmation" \
+--yesno "Press Send button to send a mail  " 8 60 
+respose=$?
+# make a decsion 
+case $respose in
+  0) 
+#OK pressed
+ if [[ $choice == *"yes"* ]]
+ then
+nohup echo "$message" |mutt -s "$subject" $mailid -a $attachment &>/dev/null & 
+ else
+nohup echo "$message" |mutt -s "$subject" $mailid &>/dev/null &
+ fi
+#nohup command &>/dev/null &
+  	;;
+  1) 
+        exit
+#  	echo "Cancel pressed." 
+  	;;
+  255) 
+#   echo "[ESC] key pressed."
+esac
+#-----------Print Progress Bar ---------------------------#
+{
+    for ((i = 0 ; i <= 100 ; i+=10)); do
+        sleep 1
+        echo $i
+    done
+} |Xdialog --title "Please wait." \
+--backtitle "ANJI BABU KAPAKAYALA" \
+--gauge "Sending....." 8 60 0
+#-----------Print info ---------------------------#
+Xdialog --title "Done." \
+--backtitle "Thanks for using ANJI BABU KAPAKAYALA Mailing Services." \
+--msgbox "Your mail has been sent . " 8 60 
+#sleep 2
+#-----------Print info ---------------------------#
+# remove $OUTPUT file
+rm $OUTPUT
+#  --cancel-label "Exit"
+#--ok-label "Send"
+#REFERENCE:  https://bash.cyberciti.biz/guide/An_input_dialog_box
+#================================================================#
+#           Written by ANJI BABU KAPAKAYALA                      #
+#================================================================#
